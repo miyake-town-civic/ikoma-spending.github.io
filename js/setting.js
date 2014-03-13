6caf26e6e621ac9085118268e4778b652077b7fa
ummtt <ummtt.git@gmail.com>
2014-03-13 16:43:29 +0900

    カラーとアイコンを生駒市版に修正

--- js/setting.js
+++ js/setting.js
@@ -10,41 +10,57 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yokohama_budget';
-OpenSpending.year = '2012';
+OpenSpending.identifier = 'ikoma_2013_ippan-kaikei';
+OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
-  '2-2': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  '3': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-1': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-2': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-3': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '4': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
-  '4-1': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
-  '4-2': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
-  '4-3': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#D33673' },
-  '5': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
-  '5-1': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
-  '5-2': { icon: 'icons/wind.svg', color: '#C75746', bcolor: '#2A3A03'  },
-  '6': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '6-1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '7': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-  '7-1': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
-  '8': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-1': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-2': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '9': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
-  '9-1': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
-  '10': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
-  '10-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
+  '1': { icon: 'icons/government.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '1-1': { icon: 'icons/government.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '2': { icon: 'icons/publicaffairs.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '2-1': { icon: 'icons/publicaffairs.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '2-2': { icon: 'icons/javni_dug.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '2-3': { icon: 'icons/books.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '2-4': { icon: 'icons/vote.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '2-5': { icon: 'icons/ekonomija.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '2-6': { icon: 'icons/payroll.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '3': { icon: 'icons/helping-others.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '3-1': { icon: 'icons/pio_mio.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '3-2': { icon: 'icons/c_kosodate_shien.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '3-3': { icon: 'icons/environment.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '3-4': { icon: 'icons/c_bousai.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '3-5': { icon: 'icons/health.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '4': { icon: 'icons/health.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '4-1': { icon: 'icons/civilian-action.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '4-2': { icon: 'icons/waste.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '5': { icon: 'icons/sangyou3.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '5-1': { icon: 'icons/c_nougyou.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '5-2': { icon: 'icons/c_syoukougyou.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '6': { icon: 'icons/c_construction.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '6-1': { icon: 'icons/misc-services.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '6-2': { icon: 'icons/our-streets.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '6-3': { icon: 'icons/machi.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '6-4': { icon: 'icons/housing.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '6-5': { icon: 'icons/c_waterdoun.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '7': { icon: 'icons/c_shyoubou.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '7-1': { icon: 'icons/c_shyoubou.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '8': { icon: 'icons/schools.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '8-1': { icon: 'icons/publicaffairs.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '8-2': { icon: 'icons/primary.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '8-3': { icon: 'icons/post-secondary.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '8-4': { icon: 'icons/pre-school.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '8-5': { icon: 'icons/civilian-action.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '8-6': { icon: 'icons/c_kenkou.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '9': { icon: 'icons/c_bousai.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '9-1': { icon: 'icons/coal.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '9-2': { icon: 'icons/forest.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '10': { icon: 'icons/financial-admin.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '10-1': { icon: 'icons/financial-admin.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '11': { icon: 'icons/japanese-money.svg', color: '#75BA0E', bcolor: '#75BA0E' },
+  '11-1': { icon: 'icons/japanese-money.svg', color: '#75BA0E', bcolor: '#75BA0E' },
 };
 
 
